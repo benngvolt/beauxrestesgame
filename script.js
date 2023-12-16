939c288c9bebe0fa96370f79638f1d9811f257aa
Benjamin Gibert <contact@benjamingibert.com>
2023-12-16 15:57:15 +0100

    nouveau commit split

--- script.js
+++ script.js
@@ -41,20 +41,21 @@ const items = [
 ]
 
 
-const chosenName = document.getElementById("chosenName");
-const chosenNumber = document.getElementById("chosenNumber");
+// const selectedName = document.getElementById("selectedName");
+// const selectedNumber = document.getElementById("selectedNumber");
 const testButton = document.getElementById("testButton");
 const resetButton = document.getElementById("resetButton");
 let selectedNameIndex;
 let selectedNumberIndex;
-let selectedNameElement;
-let selectedNumberElement;
+// let selectedNameElement;
+// let selectedNumberElement;
 
 // function compareRandom() {
 //     return Math.random() - 0.5;
 // }
 
-const namesAndNumbersList = document.querySelector(".namesAndNumbers");
+const numbersGridList = document.querySelector(".numbersGrid");
+const namesColumnList = document.querySelector(".namesList");
 
 const nameLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
 const numberNumbers = ['1', '2', '3', '4', '5', '6', '7'];
@@ -63,56 +64,49 @@ const numberNumbers = ['1', '2', '3', '4', '5', '6', '7'];
 items.map(function(item, index) {
     
     const name = document.createElement("li");
-    name.classList.add("name", "nameDisplayOn", `${item.id}`, "transparent");
-    name.classList.remove("plenty");
+    name.classList.add("name", `${item.id}`, "selectedNameDisplayOff", "transparent");
+    name.classList.remove("selectedNameDisplayOn", "plenty");
     name.textContent = item.name;
+    
     name.addEventListener("click", function(event) {
-        if (chosenName.classList.contains("chosenNameDisplayOn")) {
-            chosenName.textContent = null;
-            chosenName.classList.add("chosenNameDisplayOff");
-            chosenName.classList.remove("chosenNameDisplayOn");
-            selectedNameElement.classList.remove("nameDisplayOff", "filteredName");
-            selectedNameElement.classList.add("nameDisplayOn");
+        const existingSelectedNameElement = document.querySelector('.selectedNameDisplayOn');
+        if (existingSelectedNameElement) { 
+            existingSelectedNameElement.textContent = null;
+            existingSelectedNameElement.classList.add("selectedNameDisplayOff");
+            existingSelectedNameElement.classList.remove("selectedNameDisplayOn");
         };
-        selectedNameElement = event.currentTarget;
-        selectedNameIndex = selectedNameElement.getAttribute("data-index");
-        chosenName.textContent = item.name
-        chosenName.setAttribute("data-index", selectedNameIndex);
-        chosenName.classList.remove("chosenNameDisplayOff", "transparent", "mains", "double", "menace", "ruade", "bruit", "trois", "simple");
-        chosenName.classList.add("chosenNameDisplayOn", `${item.id}`, "plenty");
-        selectedNameElement.classList.add("nameDisplayOff");
-        selectedNameElement.classList.remove("nameDisplayOn");
+
+        selectedNameIndex = name.getAttribute("data-index");
+        name.textContent = item.name
+        name.setAttribute("data-index", selectedNameIndex);
+        name.classList.remove("selectedNameDisplayOff", "transparent", "mains", "double", "menace", "ruade", "bruit", "trois", "simple");
+        name.classList.add("selectedNameDisplayOn", `${item.id}`, "plenty");
 
     });
     name.setAttribute("data-index", index);
-    namesAndNumbersList.appendChild(name);
+    namesColumnList.appendChild(name);
 
     const number = document.createElement("li");
     const numberImage = document.createElement("img");
-    number.classList.add("number", "numberDisplayOn");
+    const numberText = document.createElement("p");
+    number.classList.add("number", "selectedNumberDisplayOff");
+    numberText.classList.add("numberText");
+    numberText.textContent = item.number;
     number.addEventListener("click", function(event) {
-        if (chosenNumber.classList.contains("chosenNumberDisplayOn")) {
-            chosenNumber.textContent = null;
-            
-            chosenNumber.classList.add("chosenNumberDisplayOff");
-            chosenNumber.classList.remove("chosenNumberDisplayOn");
-            selectedNumberElement.classList.remove("numberDisplayOff", "filteredNumber");
-            selectedNumberElement.classList.add("numberDisplayOn");
+        const existingSelectedNumberElement = document.querySelector('.selectedNumberDisplayOn');
+        if (existingSelectedNumberElement) {
+            existingSelectedNumberElement.classList.add("selectedNumberDisplayOff");
+            existingSelectedNumberElement.classList.remove("selectedNumberDisplayOn");
         };
-        selectedNumberElement = event.currentTarget;
-        selectedNumberIndex = selectedNumberElement.getAttribute("data-index");
-        chosenNumber.textContent = item.number;
-        chosenNumber.setAttribute("src", `assets/${item.number}.jpg`);
-        chosenNumber.setAttribute("data-index", selectedNumberIndex);
-        chosenNumber.classList.add("chosenNumberDisplayOn", `${item.id}`);
-        chosenNumber.classList.remove("chosenNumberDisplayOff");
-        selectedNumberElement.classList.add("numberDisplayOff");
-        selectedNumberElement.classList.remove("numberDisplayOn");
-        console.log(selectedNumberElement);
+        selectedNumberIndex = number.getAttribute("data-index"); 
+        number.setAttribute("data-index", selectedNumberIndex);
+        number.classList.add("selectedNumberDisplayOn", `${item.id}`);
+        number.classList.remove("selectedNumberDisplayOff");
     });
     number.setAttribute("data-index", index);
-    namesAndNumbersList.appendChild(number);
+    numbersGridList.appendChild(number);
     number.appendChild(numberImage);
+    number.appendChild(numberText);
     numberImage.setAttribute("src", `assets/${item.number}.jpg`);
     numberImage.setAttribute("class", "numberImage");
     
@@ -148,73 +142,95 @@ resetButton.addEventListener("click", function() {
 });
 
 function reset () {
-    chosenNumber.textContent = null;
-    chosenName.textContent = null;
+    const numbers = document.querySelectorAll(".number");
+    const names = document.querySelectorAll(".name");
+    
+    names.forEach(name => {
+        // Vérifier si l'élément a la classe "selectedNumberDisplayOn"
+        if (name.classList.contains("selectedNameDisplayOn")) {
+            // Retirer la classe "selectedNumberDisplayOn" et ajouter la classe "selectedNumberDisplayOff"
+            name.classList.remove("selectedNameDisplayOn");
+            name.classList.add("selectedNameDisplayOff");
+        }
+        if (name.classList.contains("validatedName")) {
+            // Retirer la classe "selectedNumberDisplayOn" et ajouter la classe "selectedNumberDisplayOff"
+            name.classList.remove("validatedName");
+        }
+        if (name.textContent) {
+            name.textContent = null;
+            };
+    });
 
-    chosenName.classList.add("chosenNameDisplayOff");
-    chosenName.classList.remove("chosenNameDisplayOn");
-
-    selectedNameElement.classList.remove("nameDisplayOff", "filteredName");
-    selectedNameElement.classList.add("nameDisplayOn");
-
-    chosenNumber.classList.add("chosenNumberDisplayOff");
-    chosenNumber.classList.remove("chosenNumberDisplayOn");
-
-    selectedNumberElement.classList.remove("numberDisplayOff", "filteredNumber");
-    selectedNumberElement.classList.add("numberDisplayOn");
+    numbers.forEach(number => {
+        // Vérifier si l'élément a la classe "selectedNumberDisplayOn"
+        if (number.classList.contains("selectedNumberDisplayOn")) {
+            // Retirer la classe "selectedNumberDisplayOn" et ajouter la classe "selectedNumberDisplayOff"
+            number.classList.remove("selectedNumberDisplayOn");
+            number.classList.add("selectedNumberDisplayOff");
+        }
+        if (number.classList.contains("validatedNumber")) {
+            // Retirer la classe "selectedNumberDisplayOn" et ajouter la classe "selectedNumberDisplayOff"
+            number.classList.remove("validatedNumber");
+        }
+        
+    });
 }
 
 function doesItMatch () {
 
-    const selectedNameIndex = parseInt(chosenName.getAttribute("data-index"));
-    const selectedNumberIndex = parseInt(chosenNumber.getAttribute("data-index"));
-
-    const chosenNameText = chosenName.textContent;
-    const chosenNumberText = chosenNumber.textContent;
     
-    const foundItemByName = items.find(item => item.name === chosenNameText);
-    const foundItemByNumber = items.find(item => item.number === parseInt(chosenNumberText));
+    const selectedNumber = document.querySelector(".selectedNumberDisplayOn");
+    
+    const selectedName = document.querySelector(".selectedNameDisplayOn");
+
+    const selectedNameIndex = parseInt(selectedName.getAttribute("data-index"));
+    const selectedNumberIndex = parseInt(selectedNumber.getAttribute("data-index"));
+
+    const selectedNameText = selectedName.textContent;
+    const selectedNumberText = selectedNumber.querySelector(".numberText");
+ 
+    
+    const foundItemByName = items.find(item => item.name === selectedNameText);
+    const foundItemByNumber = items.find(item => item.number === parseInt(selectedNumberText.textContent));
+
+    console.log(foundItemByName);
+    console.log(foundItemByNumber);
 
     const selectedNameElement = document.querySelector(`.name[data-index="${selectedNameIndex}"]`);
     const selectedNumberElement = document.querySelector(`.number[data-index="${selectedNumberIndex}"]`);
   
-    if (chosenNameText && chosenNumberText) {
-
+    if (selectedNameText && selectedNumberText) {
+        
         if (foundItemByName.id===foundItemByNumber.id) {
 
-            
-            selectedNameElement.classList.remove("nameDisplayOff");
-            selectedNameElement.classList.add("filteredName", "nameDisplayOn");
-            
-            chosenName.classList.add("chosenNameDisplayOff");
-            chosenName.classList.remove("chosenNameDisplayOn");
-            
-            selectedNumberElement.classList.add("filteredNumber", "numberDisplayOn" );
-            selectedNumberElement.classList.remove("numberDisplayOff");
+            console.log("yeah");
 
-            chosenNumber.classList.add("chosenNumberDisplayOff");
-            chosenNumber.classList.remove("chosenNumberDisplayOn");
+            selectedName.classList.add("selectedNameDisplayOff");
+            selectedName.classList.remove("selectedNameDisplayOn");
+            selectedName.classList.add("validatedName");
 
-            chosenNumber.textContent = null;
-            chosenName.textContent = null;
+            selectedNumber.classList.add("selectedNumberDisplayOff");
+            selectedNumber.classList.remove("selectedNumberDisplayOn");
+            selectedNumber.classList.add("validatedNumber");
+
+            
+            selectedName.textContent = null;
             
 
         } else {
 
-            chosenName.classList.add("chosenNameDisplayOff");
-            chosenName.classList.remove("chosenNameDisplayOn");
-            console.log("Before:", selectedNameElement.classList);
-            selectedNameElement.classList.remove("nameDisplayOff");
-            selectedNameElement.classList.add("nameDisplayOn");
-            console.log("After:", selectedNameElement.classList);
+            console.log("oups");
+
+            selectedName.classList.add("selectedNameDisplayOff");
+            selectedName.classList.remove("selectedNameDisplayOn");
+
             
-            chosenNumber.classList.add("chosenNumberDisplayOff");
-            chosenNumber.classList.remove("chosenNumberDisplayOn");
-            selectedNumberElement.classList.remove("numberDisplayOff");
-            selectedNumberElement.classList.add("numberDisplayOn");
+            selectedNumber.classList.add("selectedNumberDisplayOff");
+            selectedNumber.classList.remove("selectedNumberDisplayOn");
+    
             
-            chosenNumber.textContent = null;
-            chosenName.textContent = null;
+            
+            selectedName.textContent = null;
         }
     }
 };
